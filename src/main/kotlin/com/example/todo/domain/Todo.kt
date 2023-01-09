953bddfa095a601b0966484c1c70a4d99bb0bee1
twoosky <lhjae3@naver.com>
2023-01-10 01:05:57 +0900

    feat: todo entity 생성

---
+++ src/main/kotlin/com/example/todo/domain/Todo.kt
@@ -0,0 +1,19 @@
+package com.example.todo.domain
+
+import com.example.todo.domain.enum.Status
+import javax.persistence.*
+
+@Entity
+@Table(name = "todo")
+class Todo (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val title: String,
+
+    val content: String,
+
+    @Enumerated(EnumType.STRING)
+    val status: Status
+) : BaseTimeEntity()
